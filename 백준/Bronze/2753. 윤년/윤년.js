b0848171b55d9ec953c0598d66b7855eabafff04
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-04-10 17:06:01 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,5 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const year = Number(input[0]);
+
+if(year%400 == 0 || year%4==0&&year%100!=0) console.log('1')
+else console.log('0')
