fbe8f28a1a5aa89cd79d48f06f46a8f9d0315835
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-05-22 15:57:08 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,8 +1,8 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 const score = Number(input[0]);
 
-if(score>=90) console.log('A')
-else if(score<90 && score>=80) console.log('B')
-else if(score<80 && score>=70) console.log('C')
-else if(score<70 && score>=60) console.log('D')
-else console.log('F')
+if (score <= 100 && score > 89) console.log("A");
+else if (score < 90 && score > 79) console.log("B");
+else if (score < 80 && score > 69) console.log("C");
+else if (score < 70 && score > 59) console.log("D");
+else console.log("F");
