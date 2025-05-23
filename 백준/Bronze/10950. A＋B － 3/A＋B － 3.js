da7eb059fd12527dcd0b0b7dfc435664a4872be7
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-05-23 17:28:37 +0900

    [Bronze V] Title: A+B - 3, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,7 +1,8 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const num = Number(input[0]);
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-for (let i = 1; i <= num; i++) {
-    const ans = input[i].split(' ');
-    console.log(Number(ans[0]) + Number(ans[1]));
+const test = Number(input[0]);
+
+for (let i = 1; i <= test; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  console.log(a + b);
 }
