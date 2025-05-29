d6064a805dcf43e831206da8847141cfc8526751
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-05-29 17:24:32 +0900

    [Bronze III] Title: ACM 호텔, Time: 104 ms, Memory: 9884 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
@@ -1,14 +1,11 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-let testcnt = Number(input[0]);
-
-for (let i = 1; i <= testcnt; i++) {
+for (let i = 1; i <= Number(input[0]); i++) {
   let [h, w, n] = input[i].split(" ").map(Number);
+  let floor = (n % h === 0 ? h : n % h)
+  let roomNum = Math.ceil(n/h)
 
-  let floor = n % h === 0 ? h : n % h;
-  let room = Math.ceil(n / h);
+  let room = floor * 100 + roomNum
 
-  let roomNum = floor * 100 + room;
-
-  console.log(roomNum);
+  console.log(room);
 }
