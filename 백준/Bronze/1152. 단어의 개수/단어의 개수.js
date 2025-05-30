807ccfdf712a495934af2b583d64255609321bea
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-05-30 21:12:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 172 ms, Memory: 19180 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,10 +1,12 @@
-const input = require("fs").readFileSync("/dev/stdin").toString();
-
-let word = input.trim().split(" ");
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
 let cnt = 0;
 
-for (let i = 0; i < word.length; i++) {
-  if (word[i] !== "") cnt++;
+for (let i = 0; i < input.length; i++) {
+  if (input[i] !== "") cnt++;
 }
 
 console.log(cnt);
