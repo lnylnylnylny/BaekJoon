0ee3ddf51cb8c18d03348ff70c13c1a05b52c64e
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-06-11 12:02:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -0,0 +1,10 @@
+const [a, b, c] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map(Number);
+
+if (a === b && b === c) console.log(10000 + a * 1000);
+else if (a === b || a === c) console.log(1000 + a * 100);
+else if (b === c) console.log(1000 + b * 100);
+else console.log(Math.max(a, b, c) * 100);
