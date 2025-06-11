ad75180b803e31ca35acc56baa782cc1d261c319
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-06-11 19:42:17 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -0,0 +1,8 @@
+const student = require("fs").readFileSync("/dev/stdin").toString().split("\n").map(Number)
+
+const submit = new Set(student)
+const allStudent = Array.from({length : 30},(_,i) => i+1)
+
+const nosubmit = allStudent.filter((num) => !submit.has(num))
+
+console.log(nosubmit.sort((a,b) => a-b).join("\n"))
